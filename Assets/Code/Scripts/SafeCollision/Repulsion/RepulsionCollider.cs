8fccd728a7c99c395c818d66f2ce799e998015e4
reitaghchi <ren.len2426@gmail.com>
2026-02-24 18:14:59 +0900

    [update]コライダー完成。 ・反発力が侵入の度合いによって指数関数的に増加するように

--- Assets/Code/Scripts/SafeCollision/Repulsion/RepulsionCollider.cs
+++ Assets/Code/Scripts/SafeCollision/Repulsion/RepulsionCollider.cs
@@ -1,10 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 
+/// 滑らかに反発するコライダー
 /// </summary>
-[RequireComponent(typeof(Collider))]
 public class RepulsionCollider : MonoBehaviour
 {
-    
+    private Dictionary<Collider, RepulsionObject> _repulsionObjects = new();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<RepulsionObject>(out var repulsion))
+        {
+            repulsion.SetInPosition();
+            repulsion.RepulsionStateChanged?.Invoke(RepulsionState.In);
+            _repulsionObjects[other] = repulsion;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        foreach (var repulsionObject in _repulsionObjects.Values)
+        {
+            repulsionObject.RepulsionStateChanged?.Invoke(RepulsionState.Stay);
+            repulsionObject.AddRepulsionForce();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_repulsionObjects.TryGetValue(other, out var repulsion))
+        {
+            repulsion.RepulsionStateChanged?.Invoke(RepulsionState.Out);
+            _repulsionObjects.Remove(other);
+        }
+    }
 }
--- Assets/Code/Scripts/SafeCollision/Repulsion/RepulsionObject.cs
+++ Assets/Code/Scripts/SafeCollision/Repulsion/RepulsionObject.cs
@@ -1,27 +1,68 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(Rigidbody))]
 public class RepulsionObject : MonoBehaviour
 {
-    public Rigidbody Rigid;
+    [Header("反発力の基準")] [SerializeField] private float _baseRepulsiveForce = 100f;
 
-    [Header("何フレーム分の座標を記録するか")] [SerializeField]
-    private int savePositionCount = 10;
+    [Header("反発力の鋭さ（2以上で指数関数的に増加）")] [SerializeField]
+    private float _forceCurve = 2.0f;
 
-    private Queue<Vector3> Positions = new();
-    private int _frameCount = 0;
+    [Header("最大反発力")] [SerializeField] private float _maxForce = 2000f;
 
-    public void Update()
+    [Header("記録フレーム数")] [SerializeField] private int _savePositionCount = 10;
+
+    private Queue<Vector3> _positions = new();
+    private Rigidbody _rigidBody;
+    private Vector3 _inPosition;
+
+    public Action<RepulsionState> RepulsionStateChanged;
+
+    private void Start()
     {
-        Positions.Enqueue(transform.position);
-        if (Positions.Count < savePositionCount) return;
-        Positions.Dequeue();
+        _rigidBody = GetComponent<Rigidbody>();
+        // 初期位置で埋めておく
+        for (int i = 0; i < _savePositionCount; i++) _positions.Enqueue(transform.position);
     }
 
-    public Vector3 GetOldPosition()
+    private void FixedUpdate()
     {
-        return Positions.Peek();
+        //正確に指定したフレーム数だけ保存するためにFixedUpdate
+        _positions.Enqueue(transform.position);
+        if (_positions.Count > _savePositionCount) _positions.Dequeue();
     }
+
+    public void SetInPosition()
+    {
+        // 侵入した瞬間の過去の位置を固定
+        _inPosition = _positions.Peek();
+    }
+
+    public void AddRepulsionForce()
+    {
+        Vector3 diff = _inPosition - transform.position;
+        float distance = diff.magnitude;
+        if (distance < 0.001f) return;
+
+        // 反発ベクトルを正規化
+        Vector3 direction = diff.normalized;
+
+        // 指数関数的に増加する反発力の計算
+        float forceMagnitude = _baseRepulsiveForce * Mathf.Pow(distance, _forceCurve);
+
+        // 過剰な力によるオブジェクトの消失を防ぐクランプ
+        forceMagnitude = Mathf.Min(forceMagnitude, _maxForce);
+
+        _rigidBody.AddForce(direction * forceMagnitude, ForceMode.Acceleration);
+    }
+}
+
+public enum RepulsionState
+{
+    In,
+    Stay,
+    Out,
 }
