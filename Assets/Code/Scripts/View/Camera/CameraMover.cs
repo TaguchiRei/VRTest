65c623d064874620460aeab80e49fb1a5cf9f91d
reitaghchi <ren.len2426@gmail.com>
2026-04-25 16:23:58 +0900

    [update]Offsetの計算を正常に変更

--- Assets/Code/Scripts/View/Camera/CameraMover.cs
+++ Assets/Code/Scripts/View/Camera/CameraMover.cs
@@ -1,13 +1,20 @@
-using System;
 using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
+    private Vector3 _offset;
+
+    private void Awake()
+    {
+        _offset = Quaternion.Inverse(_target.rotation)
+                  * (transform.position - _target.position);
+    }
+
     public void LateUpdate()
     {
-        transform.position = _target.position;
+        transform.position = _target.position + _target.rotation * _offset;
         transform.rotation = _target.rotation;
     }
 }
