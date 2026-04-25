7a200680f089ae49eb34bebb00292afd181ef4bc
reitaghchi <ren.len2426@gmail.com>
2026-04-26 03:45:14 +0900

    Revert "[update]ApplicationでTorsoEntityを扱えるように"
    This reverts commit 7922d39b3e42fd454d224a3c8a6a674504b67f0c.

--- Assets/Code/Scripts/Application/Player/Interface/IVrMovementView.cs
+++ Assets/Code/Scripts/Application/Player/Interface/IVrMovementView.cs
@@ -11,7 +11,5 @@ namespace Application
         void AddForce(Vector3 force, ForceMode mode);
         void UpdateLeftHand(Vector3 position, Quaternion rotation);
         void UpdateRightHand(Vector3 position, Quaternion rotation);
-        void UpdateTorso(Vector3 position, Quaternion rotation);
-        void UpdateHeadLocalRotation(Quaternion localRotation);
     }
 }
