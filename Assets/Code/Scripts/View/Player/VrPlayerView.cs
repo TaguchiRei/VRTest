1aa39c3398a11993109262fbae320677c2b3bd40
reitaghchi <ren.len2426@gmail.com>
2026-04-25 18:32:35 +0900

    [update]体の動きを自然になるよう修正

--- Assets/Code/Scripts/View/Player/VrPlayerView.cs
+++ Assets/Code/Scripts/View/Player/VrPlayerView.cs
@@ -6,6 +6,7 @@ public class VrPlayerView : InitializableMonoBehaviour, IVrMovementView
 {
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Transform _neckTransform;
+    [SerializeField] private Transform _headTransform;
     [SerializeField] private Transform _leftHandTransform;
     [SerializeField] private Transform _rightHandTransform;
 
@@ -14,6 +15,8 @@ public class VrPlayerView : InitializableMonoBehaviour, IVrMovementView
         get => _rigidbody.linearVelocity;
         set => _rigidbody.linearVelocity = value;
     }
+
+    public Quaternion WorldRotation => transform.rotation;
 
     public override void Initialize()
     {
@@ -26,9 +29,19 @@ public class VrPlayerView : InitializableMonoBehaviour, IVrMovementView
         _rigidbody.AddForce(force, mode);
     }
 
+    public void ShiftPosition(Vector3 worldDelta)
+    {
+        _rigidbody.MovePosition(_rigidbody.position + worldDelta);
+    }
+
     public void UpdateNeckRotation(Quaternion rotation)
     {
         _neckTransform.localRotation = rotation;
+    }
+
+    public void UpdateHeadRotation(Quaternion rotation)
+    {
+        _headTransform.localRotation = rotation;
     }
 
     public void UpdateLeftHand(Vector3 position, Quaternion rotation)
