8093a981b6ac82d28e4f6fc87fbd1a81bf0d455f
reitaghchi <ren.len2426@gmail.com>
2026-04-26 11:57:48 +0900

    [update]Viewに反映できるように

--- Assets/Code/Scripts/View/Player/VrPlayerView.cs
+++ Assets/Code/Scripts/View/Player/VrPlayerView.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class VrPlayerView : InitializableMonoBehaviour, IVrMovementView
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private Transform _cameraTransform;
     [SerializeField] private Transform _leftHandTransform;
     [SerializeField] private Transform _rightHandTransform;
+    [SerializeField] private Transform _modelRoot;
+    [SerializeField] private Transform _neckBone;
 
     public Vector3 Velocity
     {
@@ -26,9 +27,17 @@ public class VrPlayerView : InitializableMonoBehaviour, IVrMovementView
         _rigidbody.AddForce(force, mode);
     }
 
-    public void CameraRotation(Quaternion rotation)
+    public void OnHmdRotate(NeckTransform neckTransform)
     {
-        _cameraTransform.rotation = rotation;
+        _neckBone.position = neckTransform.NeckPosition;
+        _neckBone.rotation = neckTransform.NeckRotation;
+
+        Vector3 currentEuler = _modelRoot.rotation.eulerAngles;
+        float bodyYaw = neckTransform.BodyRotationY;
+
+        float newYaw = currentEuler.y + bodyYaw;
+
+        _modelRoot.rotation = Quaternion.Euler(currentEuler.x, newYaw, currentEuler.z);
     }
 
     public void UpdateLeftHand(Vector3 position, Quaternion rotation)
